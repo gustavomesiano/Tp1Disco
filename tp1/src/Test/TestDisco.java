95026eedb458baa2c5fad268cd39f233a1372004
GustavoMesiano <gustavomesiano@hotmail.es>
2018-02-19 11:48:50 -0300

    archivo tp terminado

---
+++ tp1/src/Test/TestDisco.java
@@ -0,0 +1,22 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ar.edu.unlam.verano.pb2.disco.Disco;
+
+
+public class TestDisco {
+
+	private Boolean resultado;
+
+	@Test
+	public void testCrearDisco(){
+		
+		Disco miDisco= new Disco(30.00, 50.00);
+		resultado = miDisco.verificarDisco();
+		assertTrue(resultado);
+	}
+
+}
